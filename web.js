112b95126a9258b2a512a909c2c6277bc3f746e3
Dean Symington <dean@stokedproductions.co.za>
2013-07-08 07:51:14 +0000

    update web.js to read a template

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+ var fileContents = fs.readFile('index.html');
+ response.send(fileContents.toString());
 });
 
 var port = process.env.PORT || 5000;
